649055031180ff2c5621937cdf8aea3794f84375
NickSica <njs82@drexel.edu>
2023-03-08 13:00:55 -0500

    Changes create table

--- dc_assignment/pt_pre/create_table.sh
+++ dc_assignment/pt_pre/create_table.sh
@@ -1,6 +1,21 @@
-TOP_LIST="s1238 s15850 s35932 s386 s9234"
-
+TOP_LIST="s386 s1238 s9234 s15850 s35932"
+# 100MHz
 echo "\\begin{tabular}{| Design | Area | Power | WNS | TNS | Cell Count | Num of violating paths |}" > table.tex
+echo "\\caption{100MHz DC logic synthesis results.}" >> table.tex
+echo "    \\hline" >> table.tex
+
+for top in $TOP_LIST; do
+	report_dir=designs/$top/reports
+	dc_report_dir=../dc_synth/$report_dir
+	source <(gawk -f find_tns.awk $report_dir/pt_qor.rpt)
+	power="$(sed -n 'x;$p' $dc_report_dir/synth_power.rpt | gawk -- '{ print $(NF-1) }')"
+	echo "    ${top}.v & $area & $power & $wns & $tns & $cell_count & $num_violating \\\\" >> table.tex
+	echo "    \\hline" >> table.tex
+done
+echo "\\end{tabular}" >> table.tex
+
+# 500MHz
+echo "\\begin{tabular}{| Design | Area | Power | WNS | TNS | Cell Count | Num of violating paths |}" >> table.tex
 echo "\\caption{500MHz DC logic synthesis results.}" >> table.tex
 echo "    \\hline" >> table.tex
 
@@ -14,18 +29,3 @@ for top in $TOP_LIST; do
 done
 echo "\\end{tabular}" >> table.tex
 echo "" >> table.tex
-
-# 100MHz
-echo "\\begin{tabular}{| Design | Area | Power | WNS | TNS | Cell Count | Num of violating paths |}" >> table.tex
-echo "\\caption{100MHz DC logic synthesis results.}" >> table.tex
-echo "    \\hline" >> table.tex
-
-for top in $TOP_LIST; do
-	report_dir=designs/$top/reports
-	dc_report_dir=../dc_synth/$report_dir
-	source <(gawk -f find_tns.awk $report_dir/pt_qor.rpt)
-	power="$(sed -n 'x;$p' $dc_report_dir/synth_power.rpt | gawk -- '{ print $(NF-1) }')"
-	echo "    ${top}.v & $area & $power & $wns & $tns & $cell_count & $num_violating \\\\" >> table.tex
-	echo "    \\hline" >> table.tex
-done
-echo "\\end{tabular}" >> table.tex
